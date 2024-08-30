016bf218d4e333323667c78556f36ed6d4bf3447
Tobias Wai <jacky115980726@gmail.com>
2024-08-30 15:33:49 +0800

    created django framework

---
+++ FYP/logs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from profiles.models import Profile
+
+# Create your models here.
+
+class Log(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='logs')
+    is_correct = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f"Log of {self.profile.id}"
